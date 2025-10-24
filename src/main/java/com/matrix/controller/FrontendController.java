c7c14189321efb12ca81a560b7b310aadf326a23
eswari0027 <119641422+eswari0027@users.noreply.github.com>
2025-10-24 13:20:44 +0530

    Update FrontendController.java

--- src/main/java/com/matrix/controller/FrontendController.java
+++ src/main/java/com/matrix/controller/FrontendController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FrontendController {
 
-    @GetMapping({"/", "/Matrixglobetech"})
+    @GetMapping("/matrixglobetech") 
     public String home(Model model) {
         model.addAttribute("welcomeMessage", "Matrix Globe Tech");
         return "matrixgloltech/index";
