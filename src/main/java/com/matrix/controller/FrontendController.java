7dac51418bf01d927a2084b1d287a7f917f03e6e
Jayashree054 <jayashree054@gmail.com>
2025-10-24 13:13:37 +0530

    Move local code to feature-new1 branch

--- src/main/java/com/matrix/controller/FrontendController.java
+++ src/main/java/com/matrix/controller/FrontendController.java
@@ -3,13 +3,13 @@ package com.matrix.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class FrontendController {
 
-    @GetMapping("/Matrixglobetech")
+    @GetMapping({"/", "/Matrixglobetech"})
     public String home(Model model) {
         model.addAttribute("welcomeMessage", "Matrix Globe Tech");
-        return "matrixgloltech/index"; // template folder: templates/matrixgloltech/index.html
+        return "matrixgloltech/index";
     }
+
 }
