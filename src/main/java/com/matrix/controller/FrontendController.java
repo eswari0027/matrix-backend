fe7fa477132ee408334fa9f36069921365a5825b
eswari0027 <119641422+eswari0027@users.noreply.github.com>
2025-10-24 13:39:32 +0530

    Update FrontendController.java

--- src/main/java/com/matrix/controller/FrontendController.java
+++ src/main/java/com/matrix/controller/FrontendController.java
@@ -5,11 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequestMapping("/frontend")  // Add a prefix
 public class FrontendController {
 
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("welcomeMessage", "Matrix Globe Tech");
-        return "matrixgloltech/index"; // template folder: templates/matrixgloltech/index.html
+        return "matrixgloltech/index";
     }
 }
